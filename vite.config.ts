dd318362a8cf6e101830848cc7541b31b6d12c8e
Aslan <korpeevaslan@gmail.com>
2025-02-08 04:49:24 +0500

    Add GitHub Pages deployment support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {fileURLToPath} from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/on_esports_test_task/",
   resolve: {
     alias: [
-        { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-    ]
-  }
-})
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
+});
